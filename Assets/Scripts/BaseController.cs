50149ce234bc99c6f3539acfee02aab5a3cb46f6
ckdals652 <ckdals652@naver.com>
2025-05-09 12:30:08 +0900

    feat : 플레이어 이동,점프 구현

--- Assets/Scripts/BaseController.cs
+++ Assets/Scripts/BaseController.cs
@@ -4,44 +4,75 @@ using UnityEngine;
 
 public class BaseController : MonoBehaviour
 {
-    protected Rigidbody rigidBody;
+    protected Rigidbody2D rigidBody;
     protected SpriteRenderer spriteRenderer;
 
-    [SerializeField] protected float speed = 3f;
+    protected float speed = 12f;
 
-    [SerializeField] protected float jampPower = 3f;
-    [SerializeField] protected int jampCount = 3;
+    protected float jumpPower = 15f;
+    protected int OriginJumpCount = 2;
+    int jumpCount;
 
-    [SerializeField] protected float hp = 100f;
+    [SerializeField] protected LayerMask groundLayer;
+    [SerializeField] protected Transform groundCheckPoint;
 
-    [SerializeField] protected float attackSpeed = 1f;
-    [SerializeField] protected float attackPower = 3f;
+    protected float hp = 100f;
+
+    protected float attackSpeed = 1f;
+    protected float attackPower = 3f;
 
     public void Start()
     {
         //자기 컴포넌트 가져오기
-        rigidBody = GetComponent<Rigidbody>();
+        rigidBody = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
+
+        jumpCount = OriginJumpCount;
     }
 
+    public virtual void Jump()
+    {
+        //땅에 닿을때 jumpCount 초기화
+        if (IsGrounded())
+        {
+            //Debug.Log("isground");
+            jumpCount = OriginJumpCount;
+        }
+        // 점프 입력(왜인지모름 왜 설정한거보다 한번 더 점프하는지)
+        if (Input.GetKeyDown(KeyCode.Space) && jumpCount > 1)
+        {
+            rigidBody.velocity = new Vector2(rigidBody.velocity.x, jumpPower);
+            jumpCount--;
+            //Debug.Log(jumpCount);
+        }
+    }
+    public bool IsGrounded()
+    {
+        float checkDistance = 0.2f;
+        RaycastHit2D hit = Physics2D.Raycast(groundCheckPoint.position, Vector2.down, checkDistance, groundLayer);
+        //Debug.Log(hit.collider.name);
+        //확인용
+        Debug.DrawRay(groundCheckPoint.position, Vector2.down * checkDistance, Color.red);
 
-
-    public void Jamp()
+        return hit.collider != null;
+    }
+    public virtual void JumpPowerUpDown(float num)
     {
 
     }
-    public void JampPowerUpDown(float num)
+
+    public virtual void Attack()
+    {
+
+    }
+    public virtual void AttackPowerUpDown(float num)
     {
 
     }
 
-    public void Attack()
+    public void AutoMove()
     {
-
+        // x축 자동 이동
+        rigidBody.velocity = new Vector2(speed, rigidBody.velocity.y);
     }
-    public void AttackPowerUpDown(float num)
-    {
-
-    }
-
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,8 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Timeline;
 
 public class Player : BaseController
 {
+    private Animator playerAnimator;
 
+    public void Start()
+    {
+        base.Start();
+        playerAnimator = GetComponentInChildren<Animator>();
+        playerAnimator.SetBool("IsRun", true);
+    }
+    public void Update()
+    {
+        Jump();
+    }
+
+    public void FixedUpdate()
+    {
+        AutoMove();
+    }
+
+    public override void Jump()
+    {
+        base.Jump();
+
+        if (IsGrounded())
+        {
+            playerAnimator.SetBool("IsJump", false);
+        }
+        else
+        {
+            playerAnimator.SetBool("IsJump", true);
+        }
+    }
 }
