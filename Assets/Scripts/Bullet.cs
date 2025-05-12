f1f9b8f4c8d4dd13eb38aec0396815e2fbeb1084
ckdals652 <ckdals652@naver.com>
2025-05-12 11:52:18 +0900

    wip : 오브젝트 풀링을 활용한 총알 발사

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,23 +4,38 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    [SerializeField] private Player player;
+
     private SpriteRenderer spriteRenderer;
     private Rigidbody2D rigidbody;
 
-    private float bulletSpeed = 1f;
+    private Vector2 shotDirection;
+
+    private float bulletSpeed = 1000f;
 
     public void Start()
     {
         spriteRenderer = GetComponentInChildren<SpriteRenderer>();
         rigidbody = GetComponentInChildren<Rigidbody2D>();
     }
-    
-    //총알 전진
-    public void BulletAdvance(Vector2 vector)
+
+    public void Update()
     {
-        rigidbody.velocity = vector.normalized*bulletSpeed;
+        BulletAdvance();
     }
-    
+
+    //총알 전진
+    public void BulletAdvance()
+    {
+        rigidbody.velocity = shotDirection * bulletSpeed;
+    }
+
+    //총알 방향
+    public void SetDirection()
+    {
+        shotDirection = player.SetClickDirection();
+    }
+
     //총알 속도 증감
     public void BulletSpeedUpDown(float num)
     {
@@ -31,8 +46,8 @@ public class Bullet : MonoBehaviour
     {
         if (collision != null)
         {
-            //뭐라도 부딧치면 비활성화
-            gameObject.SetActive(false);
+            //뭐라도 부딪치면 비활성화
+            //gameObject.SetActive(false);
         }
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Tilemaps;
 using UnityEngine;
 using UnityEngine.Timeline;
 
@@ -7,7 +8,10 @@ public class Player : BaseController
 {
     private Animator playerAnimator;
     private BoxCollider2D playerBoxCollider2D;
-    Vector2 orignBoxSize;
+    private Vector2 orignBoxSize;
+    [SerializeField] private Transform bulletStartPosition;
+
+    public PoolManager poolManager;
 
     protected float attackSpeed = 1f;
     protected float attackPower = 3f;
@@ -24,11 +28,15 @@ public class Player : BaseController
     {
         Jump();
         Silde();
-    }
 
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Attack(SetClickDirection());
+        }
+    }
     public void FixedUpdate()
     {
-        AutoMove();
+        //AutoMove();
     }
 
     public override void Jump()
@@ -65,12 +73,38 @@ public class Player : BaseController
         this.hp += num;
     }
 
-    public void Attack()
+    //마우스 위치 월드 좌표와 플레이어 위치를 연결한 백터값의 노멀라이즈 값을 구해주는 함수
+    public Vector2 SetClickDirection()
     {
+        Vector2 shotDirection = Vector2.zero;
 
+        //마우스 포지션을 월드 좌표로 변환하기
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = 0;
+        //Debug.Log(mousePos);
+        shotDirection = mousePos - bulletStartPosition.position;
+
+        return shotDirection.normalized;
     }
+
+    //오브젝트 풀에서 총알 가져와서 쏘기
+    public void Attack(Vector2 vector)
+    {
+        GameObject bullet = poolManager.GetBullet();
+        bullet.transform.position = bulletStartPosition.position;
+        bullet.SetActive(true);
+
+        Bullet bulletScript = GetComponent<Bullet>();
+
+        if (bulletScript != null && bullet.activeInHierarchy)
+        {
+            bulletScript.SetDirection();
+        }
+    }
+
+    //공격력 증감
     public void AttackPowerUpDown(float num)
     {
-
+        attackPower += num;
     }
 }
